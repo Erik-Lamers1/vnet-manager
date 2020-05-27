7686a2ac150edd4041f1c9b14ceeab1978e3478e
Erik Lamers <lamerse95@gmail.com>
2020-05-27 13:07:27 +0200

    Add storage operations

---
+++ vnet_manager/operations/storage.py
@@ -0,0 +1,36 @@
+from logging import getLogger
+from pylxd.exceptions import LXDAPIException
+
+from vnet_manager.providers.lxc import get_lxd_client
+from vnet_manager.conf import settings
+
+logger = getLogger(__name__)
+
+
+def check_for_lxc_storage_pool(name=settings.LXC_STORAGE_POOL_NAME):
+    """
+    Check for the existence of the LXC storage pool defined in the settings
+    :param str name: The name of the storage pool to check for (default from the settings)
+    :return: bool: True if the pool exists, False otherwise
+    """
+    logger.debug("Checking for the existence of LXC {} storage pool".format(name))
+    return get_lxd_client().storage_pools.exists(settings.LXC_STORAGE_POOL_NAME)
+
+
+def create_lxc_storage_pool(name=settings.LXC_STORAGE_POOL_NAME, driver=settings.LXC_STORAGE_POOL_DRIVER):
+    """
+    Creates a new LXC storage pool with the passed driver and name
+    :param str name: The name of the storage pool to create (default from settings)
+    :param str driver: The driver to use (default from settings)
+    """
+    # Sanity check, we don't want to create duplicate pools
+    if check_for_lxc_storage_pool(name):
+        raise RuntimeError("LXC storage pool {} already exists, cannot create duplicate".format(name))
+
+    # Make it
+    client = get_lxd_client()
+    try:
+        client.storage_pools.create({"name": name, "driver": driver})
+        logger.info("Storage pool {} with driver {} successfully created".format(name, driver))
+    except LXDAPIException as e:
+        logger.error("Got API error while creating storage pool {}. Error: {}".format(name, e))
