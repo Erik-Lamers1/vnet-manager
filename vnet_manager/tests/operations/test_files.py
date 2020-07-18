279bf8baaeb3ff761dd661a1bf9bbc2ddb0bd5bf
Erik Lamers <lamerse95@gmail.com>
2020-07-18 11:49:26 +0200

    Add tests for select_files_and_put_on_machine

--- vnet_manager/tests/operations/test_files.py
+++ vnet_manager/tests/operations/test_files.py
@@ -1,8 +1,9 @@
 from copy import deepcopy
 from unittest.mock import call
+from os.path import join
 
 from vnet_manager.tests import VNetTestCase
-from vnet_manager.operations.files import put_files_on_machine
+from vnet_manager.operations.files import put_files_on_machine, select_files_and_put_on_machine
 from vnet_manager.conf import settings
 
 
@@ -26,3 +27,48 @@ class TestPutFilesOnMachine(VNetTestCase):
         del self.config["machines"]["router101"]
         put_files_on_machine(self.config)
         self.assertFalse(self.select_and_put_on_machine.called)
+
+
+class TestSelectFilesAndPutOnMachine(VNetTestCase):
+    def setUp(self) -> None:
+        self.logger = self.set_up_patch("vnet_manager.operations.files.logger")
+        self.is_dir = self.set_up_patch("vnet_manager.operations.files.isdir")
+        self.is_dir.return_value = True
+        self.is_file = self.set_up_patch("vnet_manager.operations.files.isfile")
+        self.is_file.return_value = True
+        self.place_file_on_lxc_machine = self.set_up_patch("vnet_manager.operations.files.place_file_on_lxc_machine")
+        self.list_dir = self.set_up_patch("vnet_manager.operations.files.listdir")
+        self.list_dir.return_value = ["file1", "file2", "file3"]
+        self.machine = "router100"
+        self.files = settings.VALIDATED_CONFIG["machines"]["router100"]["files"]
+
+    def test_select_files_and_put_on_machine_calls_check_methods_to_check_for_file_or_dir(self):
+        self.is_dir.return_value = False
+        select_files_and_put_on_machine(self.machine, self.files, "lxc")
+        self.is_dir.assert_called_once_with(next(iter(self.files)))
+        self.is_file.assert_called_once_with(next(iter(self.files)))
+
+    def test_select_files_and_put_on_machine_put_a_single_file_on_machine(self):
+        self.is_dir.return_value = False
+        select_files_and_put_on_machine(self.machine, self.files, "lxc")
+        self.place_file_on_lxc_machine.assert_called_once_with(self.machine, next(iter(self.files)), next(iter(self.files.values())))
+
+    def test_select_files_and_put_on_machine_calls_listdir_function(self):
+        select_files_and_put_on_machine(self.machine, self.files, "lxc")
+        self.list_dir.assert_called_once_with(next(iter(self.files)))
+
+    def test_select_files_and_put_on_machine_calls_place_method_for_each_file_in_dir(self):
+        select_files_and_put_on_machine(self.machine, self.files, "lxc")
+        calls = [
+            call(self.machine, join(next(iter(self.files)), file), join(next(iter(self.files.values())), file))
+            for file in self.list_dir.return_value
+        ]
+        self.place_file_on_lxc_machine.assert_has_calls(calls)
+
+    def test_select_files_and_put_on_machine_logs_error_if_file_is_not_a_dir_or_file(self):
+        self.is_dir.return_value = False
+        self.is_file.return_value = False
+        select_files_and_put_on_machine(self.machine, self.files, "lxc")
+        self.logger.error.assert_called_once_with(
+            "Tried to select file {} for copying, but it is neither a file nor a directory".format(next(iter(self.files)))
+        )
