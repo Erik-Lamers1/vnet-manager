e352ece47805cb874b2d848bd353a98fb6c3be31
Erik Lamers <lamerse95@gmail.com>
2020-08-01 13:50:32 +0200

    Add tests for destroy_lxc_image

--- vnet_manager/tests/operations/test_image.py
+++ vnet_manager/tests/operations/test_image.py
@@ -2,7 +2,7 @@ from unittest.mock import MagicMock
 from pylxd.exceptions import NotFound
 
 from vnet_manager.tests import VNetTestCase
-from vnet_manager.operations.image import check_if_lxc_image_exists, create_lxc_image_from_container
+from vnet_manager.operations.image import check_if_lxc_image_exists, create_lxc_image_from_container, destroy_lxc_image
 
 
 class TestCheckIfLXCImageExists(VNetTestCase):
@@ -86,3 +86,46 @@ class TestCreateLXCImageFromContainer(VNetTestCase):
     def test_create_lxc_image_from_container_add_alias_when_requested(self):
         create_lxc_image_from_container("test", alias="test_alias", description="test_desc")
         self.image.add_alias.assert_called_once_with("test_alias", "test_desc")
+
+
+class TestDestroyLXCImage(VNetTestCase):
+    def setUp(self) -> None:
+        self.check_if_image_exists = self.set_up_patch("vnet_manager.operations.image.check_if_lxc_image_exists")
+        self.get_lxd = self.set_up_patch("vnet_manager.operations.image.get_lxd_client")
+        self.client = MagicMock()
+        self.get_lxd.return_value = self.client
+        self.image = MagicMock()
+        self.client.images.get_by_alias.return_value = self.image
+        self.client.images.get.return_value = self.image
+
+    def test_destroy_lxc_image_checks_if_image_exists(self):
+        destroy_lxc_image("test")
+        self.check_if_image_exists.assert_called_once_with("test", by_alias=True)
+
+    def test_destroy_lxc_image_check_if_image_exists_without_alias(self):
+        destroy_lxc_image("test", by_alias=False)
+        self.check_if_image_exists.assert_called_once_with("test", by_alias=False)
+
+    def test_destroy_lxc_image_calls_get_lxd_client(self):
+        destroy_lxc_image("test")
+        self.get_lxd.assert_called_once_with()
+
+    def test_destroy_lxc_image_calls_images_get_by_alias_by_default(self):
+        destroy_lxc_image("test")
+        self.client.images.get_by_alias.assert_called_once_with("test")
+        self.assertFalse(self.client.images.get.called)
+
+    def test_destroy_lxc_image_calls_images_get_when_not_by_alias(self):
+        destroy_lxc_image("test", by_alias=False)
+        self.client.images.get.assert_called_once_with("test")
+        self.assertFalse(self.client.images.get_by_alias.called)
+
+    def test_destroy_lxc_image_does_nothing_when_image_does_not_exist(self):
+        self.check_if_image_exists.return_value = False
+        destroy_lxc_image("test")
+        self.assertFalse(self.get_lxd.called)
+        self.assertFalse(self.image.delete.called)
+
+    def test_destroy_lxc_image_calls_image_delete_method(self):
+        destroy_lxc_image("test")
+        self.image.delete.assert_called_once_with()
