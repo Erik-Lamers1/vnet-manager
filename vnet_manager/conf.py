628a6273755c576c2e0decc8f5b2c6d8f454517d
Erik Lamers <erik@byte.nl>
2020-08-25 14:21:03 +0200

    Remove six

--- vnet_manager/conf.py
+++ vnet_manager/conf.py
@@ -1,4 +1,3 @@
-import six
 import collections
 import importlib
 import os
@@ -56,7 +55,7 @@ def perform_import(val, setting_name):
     If the given setting is a string import notation,
     then perform the necessary import or imports.
     """
-    if isinstance(val, six.string_types):
+    if isinstance(val, str):
         return import_from_string(val, setting_name)
     if isinstance(val, (list, tuple)):
         return [import_from_string(item, setting_name) for item in val]
