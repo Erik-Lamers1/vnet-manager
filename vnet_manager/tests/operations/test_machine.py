2d607319e2543e703ade447feeea951fce49db3f
Erik Lamers <erik@byte.nl>
2020-09-12 14:22:04 +0200

    Add tests for lxc_exists_check and status_check

--- vnet_manager/tests/operations/test_machine.py
+++ vnet_manager/tests/operations/test_machine.py
@@ -1,6 +1,9 @@
+from unittest.mock import Mock
+from pylxd.exceptions import NotFound
+
 from vnet_manager.tests import VNetTestCase
 from vnet_manager.conf import settings
-from vnet_manager.operations.machine import show_status
+from vnet_manager.operations.machine import show_status, check_if_lxc_machine_exists, get_lxc_machine_status
 
 
 class TestShowStatus(VNetTestCase):
@@ -22,3 +25,41 @@ class TestShowStatus(VNetTestCase):
         self.tabulate.assert_called_once_with(
             [self.get_lxc_machine_status.return_value], headers=["Name", "Status", "Provider"], tablefmt="pretty"
         )
+
+
+class TestCheckIfLXCMachineExists(VNetTestCase):
+    def setUp(self) -> None:
+        self.machine = Mock()
+        self.lxd_client = self.set_up_patch("vnet_manager.operations.machine.get_lxd_client")
+        self.lxd_client.return_value = self.machine
+
+    def test_check_if_lxc_machine_exists_calls_exists_method(self):
+        check_if_lxc_machine_exists("test")
+        self.machine.containers.exists.assert_called_once_with("test")
+
+    def test_check_if_lxc_machine_exists_returns_value_of_exists_method(self):
+        self.machine.containers.exists.return_value = False
+        self.assertFalse(check_if_lxc_machine_exists("test"))
+
+
+class TestGetLXCMachineStatus(VNetTestCase):
+    def setUp(self) -> None:
+        self.machine = Mock()
+        self.lxd_client = self.set_up_patch("vnet_manager.operations.machine.get_lxd_client")
+        self.lxd_client.return_value = self.machine
+
+    def test_get_lxc_machine_calls_lxd_client(self):
+        get_lxc_machine_status("test")
+        self.lxd_client.assert_called_once_with()
+
+    def test_get_lxc_machine_status_calls_get_method(self):
+        get_lxc_machine_status("test")
+        self.machine.containers.get.assert_called_once_with("test")
+
+    def test_get_lxc_machine_status_returns_list_with_status_of_container(self):
+        self.machine.containers.get.return_value.status = "banaan"
+        self.assertEqual(get_lxc_machine_status("test"), ["test", "banaan", "LXC"])
+
+    def test_get_lxc_machine_status_returns_na_on_not_found_exception(self):
+        self.machine.containers.get.side_effect = NotFound(response="blaap")
+        self.assertEqual(get_lxc_machine_status("test"), ["test", "NA", "LXC"])
