5bc94c78523c01220b597107669c393903e647df
Erik Lamers <mail@eriklamers.nl>
2020-10-24 13:58:29 +0200

    Add tests for create_vnet_lxc_profile

--- vnet_manager/tests/operations/test_profile.py
+++ vnet_manager/tests/operations/test_profile.py
@@ -1,7 +1,8 @@
 from unittest.mock import Mock
 
+from vnet_manager.conf import settings
 from vnet_manager.tests import VNetTestCase
-from vnet_manager.operations.profile import check_if_lxc_profile_exists
+from vnet_manager.operations.profile import check_if_lxc_profile_exists, create_vnet_lxc_profile
 
 
 class TestCheckIfLXCProfileExists(VNetTestCase):
@@ -17,3 +18,31 @@ class TestCheckIfLXCProfileExists(VNetTestCase):
     def test_check_if_lxc_profile_exists_calls_client_profile(self):
         check_if_lxc_profile_exists("blaap")
         self.client.profiles.exists.assert_called_once_with("blaap")
+
+
+class TestCreateVNetLXCProfile(VNetTestCase):
+    def setUp(self) -> None:
+        self.client = Mock()
+        self.lxd_client = self.set_up_patch("vnet_manager.operations.profile.get_lxd_client")
+        self.lxd_client.return_value = self.client
+        self.check_if_profile_exists = self.set_up_patch("vnet_manager.operations.profile.check_if_lxc_profile_exists")
+        self.check_if_profile_exists.return_value = False
+
+    def test_create_vnet_lxc_profile_calls_get_lxd_client(self):
+        create_vnet_lxc_profile("test")
+        self.lxd_client.assert_called_once_with()
+
+    def test_create_vnet_lxc_profiles_calls_check_if_profile_exists(self):
+        create_vnet_lxc_profile("test")
+        self.check_if_profile_exists.assert_called_once_with("test")
+
+    def test_create_vnet_lxc_profile_raises_runtimeerror_when_profile_already_exists(self):
+        self.check_if_profile_exists.return_value = True
+        with self.assertRaises(RuntimeError):
+            create_vnet_lxc_profile("test")
+
+    def test_create_vnet_lxc_profile_calls_lxd_client_profile_create(self):
+        create_vnet_lxc_profile("test")
+        self.client.profiles.create.assert_called_once_with(
+            "test", config={}, devices={"root": {"path": "/", "pool": settings.LXC_STORAGE_POOL_NAME, "type": "disk"}}
+        )
