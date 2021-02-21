e590275c0078680e263c717910c5c0da9089a7b0
Erik Lamers <mail@eriklamers.nl>
2021-02-21 12:05:58 +0100

    Bump version to 0.7.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 import os
 from setuptools import setup, find_packages
 
-VERSION = "0.6.0"
+VERSION = "0.7.0"
 
 # allow setup.py to be run from any path
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
@@ -17,7 +17,15 @@ setup(
     license="MIT",
     # PyLXD 2.2.11 is currently broken: https://github.com/lxc/pylxd/issues/404
     install_requires=["colorama", "PyYAML", "tabulate", "pylxd==2.2.10", "pyroute2", "psutil", "distro"],
-    entry_points={"console_scripts": ["vnet-manager = vnet_manager.vnet_manager:main",],},
-    classifiers=["Programming Language :: Python :: 3", "License :: OSI Approved :: MIT License", "Operating System :: OS Independent",],
+    entry_points={
+        "console_scripts": [
+            "vnet-manager = vnet_manager.vnet_manager:main",
+        ],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
     python_requires=">=3.6",
 )
